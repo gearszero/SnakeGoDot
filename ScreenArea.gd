43d5bce93170ca14027b2e3d232098241f805579
gearszero <61977555+gearszero@users.noreply.github.com>
2024-03-18 17:58:16 +0100

    Initial commit

---
+++ ScreenArea.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name ScreenArea
+
+# Member variables
+var screenWidth : int
+var screenHeight : int
+
+#Class Constructor
+func _init(screenWidthValue = 16, screenHeightValue = 32):
+	screenWidth = screenWidthValue
+	screenHeight = screenHeightValue
---
+++ scenes/main.gd
@@ -0,0 +1,19 @@
+extends Node
+
+@export var snake_scene : PackedScene
+
+var gameScore : int
+var gameStarted : bool = false
+
+var screenArea = ScreenArea.new(16,32)
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
